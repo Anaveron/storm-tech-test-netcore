dc417196d91e65d014a5a4b9f8910f065064250a
Arkadiusz Wojtal <wojtalarkadiusz@gmail.com>
2020-03-09 16:51:22 +0100

    Task 8 - Added method in Gravatar class which uses Gravatar API to get username using mail (if it exists) with cache. Then modified _LoginPartial.cshtml and _DetailList.cshtml to display it alongside user email

--- Todo/Services/Gravatar.cs
+++ Todo/Services/Gravatar.cs
@@ -1,10 +1,23 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json.Linq;
 
 namespace Todo.Services
 {
     public static class Gravatar
     {
+        private static IMemoryCache Cache { get; }
+
+        static Gravatar()
+        {
+            Cache = new MemoryCache(new MemoryCacheOptions());
+        }
+
         public static string GetHash(string emailAddress)
         {
             using (var md5 = MD5.Create())
@@ -19,6 +32,43 @@ namespace Todo.Services
                 }
                 return builder.ToString().ToLowerInvariant();
             }
+        }
+
+        public static async Task<string> GetUserNameAsync(string hash, string url)
+        {
+            if (!Cache.TryGetValue(hash, out string userName))
+            {
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var uri = new Uri(url + $"/{hash}.json", UriKind.Absolute);
+
+                        client.Timeout = new TimeSpan(0, 0, 0, 30);
+                        client.DefaultRequestHeaders.Add("User-Agent", "Storm-Ideas-App");
+
+                        var response = await client.GetAsync(uri);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var stringResponse = await response.Content.ReadAsStringAsync();
+                            var jsonObject = JObject.Parse(stringResponse);
+
+                            userName = jsonObject["entry"]?.FirstOrDefault()?["preferredUsername"].ToString();
+
+                            var cacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                            Cache.Set(hash, userName, cacheOptions);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Error logging here
+                }
+            }
+
+            return userName;
         }
     }
 }
